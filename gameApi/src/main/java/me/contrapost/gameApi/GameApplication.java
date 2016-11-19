16a777cbbfc31b98deeaa668fff76d3c0959ba10
Alexander <shiale14@student.westerdals.no>
2016-11-19 20:11:29 +0100

    add gameApi module and change pom files accordingly

---
+++ gameApi/src/main/java/me/contrapost/gameApi/GameApplication.java
@@ -0,0 +1,15 @@
+package me.contrapost.gameApi;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+/**
+ * Created by alexandershipunov on 19/11/2016.
+ * AS
+ */
+public class GameApplication extends Application<GameConfiguration> {
+    @Override
+    public void run(GameConfiguration gameConfiguration, Environment environment) throws Exception {
+
+    }
+}
---
+++ gameApi/src/main/java/me/contrapost/gameApi/GameConfiguration.java
@@ -0,0 +1,10 @@
+package me.contrapost.gameApi;
+
+import io.dropwizard.Configuration;
+
+/**
+ * Created by alexandershipunov on 19/11/2016.
+ * Empty for now
+ */
+public class GameConfiguration extends Configuration {
+}
